faff4585b0676c849a712e913be11568016141cb
WeedBlya <dof1nator228@gmail.com>
2026-09-26 00:26:03 +0300

    chore: add .config/hypr/modules/colors.lua

---
+++ .config/hypr/modules/colors.lua
@@ -0,0 +1,9 @@
+Colors = {
+    base = "rgb(1e1e2e)",
+    surface0 = "rgb(313244)",
+    surface1 = "rgb(45475a)",
+    text = "rgb(cdd6f4)",
+    subtext = "rgb(a6adc8)",
+    blue = "rgb(89b4fa)",
+    mauve = "rgb(cba6f7)",
+}

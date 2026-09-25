7912187a10a64741e0f7606eda94c4c2f800096b
WeedBlya <dof1nator228@gmail.com>
2026-09-26 00:26:25 +0300

    chore: add .config/hypr/modules/permissions.lua

---
+++ .config/hypr/modules/permissions.lua
@@ -0,0 +1,6 @@
+hl.config({ ecosystem = { enforce_permissions = true } })
+
+hl.permission("/usr/(bin|local/bin)/grim", "screencopy", "allow")
+hl.permission("/usr/(lib|libexec|lib64)/xdg-desktop-portal-hyprland", "screencopy", "allow")
+hl.permission("/usr/(bin|local/bin)/wf-recorder", "screencopy", "allow")
+hl.permission("/usr/(bin|local/bin)/hyprlock", "screencopy", "allow")

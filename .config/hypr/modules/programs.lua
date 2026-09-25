79bef23db8f5c2a055ec748d60a1fdd147b8f0f1
WeedBlya <dof1nator228@gmail.com>
2026-09-26 00:26:07 +0300

    chore: add .config/hypr/modules/programs.lua

---
+++ .config/hypr/modules/programs.lua
@@ -0,0 +1,7 @@
+Programs = {
+    Terminal = "kitty",
+    FileManager = "nautilus",
+    FileManagerTUI = "kitty --app-id floats -e yazi",
+    Menu = "fuzzel --show drun",
+    Browser = "firefox",
+}

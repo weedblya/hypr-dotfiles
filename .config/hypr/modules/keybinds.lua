ce930f7d64067b5082423987c2e6320dbefb6662
WeedBlya <dof1nator228@gmail.com>
2026-09-26 00:45:44 +0300

    bind screenshot to Super Shift S

--- .config/hypr/modules/keybinds.lua
+++ .config/hypr/modules/keybinds.lua
@@ -27,5 +27,4 @@ end
 hl.bind("SUPER + mouse:272", hl.dsp.window.drag(), { mouse = true })
 hl.bind("SUPER + mouse:273", hl.dsp.window.resize(), { mouse = true })
 
-hl.bind("Print", hl.dsp.exec_cmd("~/.config/hypr/scripts/screenshot-full.sh"))
-hl.bind("SHIFT + Print", hl.dsp.exec_cmd("~/.config/hypr/scripts/screenshot-area.sh"))
+hl.bind("SUPER + SHIFT + S", hl.dsp.exec_cmd("~/.config/hypr/scripts/screenshot-area.sh"))

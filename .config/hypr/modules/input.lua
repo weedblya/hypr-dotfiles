659b866acfc24a3cda488b777abbec0ae92a868b
WeedBlya <dof1nator228@gmail.com>
2026-09-26 00:26:14 +0300

    chore: add input config

---
+++ .config/hypr/modules/input.lua
@@ -0,0 +1,14 @@
+hl.config({
+    input = {
+        kb_layout = "us,ru",
+        follow_mouse = 1,
+        sensitivity = 0,
+        touchpad = { natural_scroll = false },
+    },
+})
+
+hl.gesture({
+    fingers = 3,
+    direction = "horizontal",
+    action = "workspace",
+})
